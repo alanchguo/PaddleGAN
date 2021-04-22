36fc38cdc588249daf414d0135e0e4b482b1b330
alanchguo <alanchguo@hotmail.com>
2021-04-22 10:09:21 +0800

    Add files via upload

---
+++ 作业4_老北京城影像修复.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 19 19:43:23 2021
+
+@author: alanc
+"""
+
+# 安装ppgan
+# 当前目录在: /home/aistudio/, 这个目录也是左边文件和文件夹所在的目录
+# 克隆最新的PaddleGAN仓库到当前目录
+# !git clone https://github.com/PaddlePaddle/PaddleGAN.git
+# 如果从github下载慢可以从gitee clone：
+git clone https://gitee.com/paddlepaddle/PaddleGAN.git
+%cd PaddleGAN/
+pip install -v -e .
